c18deea327cd3b2ab1bb90f48ec487e50da99b56
Laura <milanbartky@gmail.com>
2023-07-24 12:02:39 +0200

    yes

--- app/Server.php
+++ app/Server.php
@@ -127,7 +127,7 @@ class Server
                     $assoc[$player['licenseIdentifier']] = [
                         'source'    => $player['source'],
                         'character' => $character['id'] ?? null,
-                        'characterFlags' => $character['flags'] ?? 256, // 256 = "default" flags
+                        'characterFlags' => $character['flags'] ?? 0,
                         'flags'     => $player['flags'] ?? 0,
                         'name'      => $player['playerName'] ?? null
                     ];
