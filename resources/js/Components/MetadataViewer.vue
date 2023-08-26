5ec8e7d494d988a26cacd94e38c0548b62a6c38b
Laura <milanbartky@gmail.com>
2023-08-26 20:38:42 +0200

    nah

--- resources/js/Components/MetadataViewer.vue
+++ resources/js/Components/MetadataViewer.vue
@@ -137,7 +137,7 @@ export default {
             let seconds = Math.floor(ms / 1000).toString().padEnd(2, '0');
             ms = ms % 1000;
 
-            let fmt = seconds + (ms ? `.${ms.toString().padEnd(3, '0')}` : '.0') + '<span class="text-gray-400 ml-0.5">s</span>';
+            let fmt = seconds + (ms ? `.${ms.toString().padEnd(3, '0')}` : '') + '<span class="text-gray-400 ml-0.5">s</span>';
 
             let minutes = Math.floor(seconds / 60).toString().padEnd(2, '0');
             seconds = seconds % 60;
