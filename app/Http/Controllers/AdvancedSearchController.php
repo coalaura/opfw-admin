d4af2d776613d875c2880121ce17d5a01e7d8b52
Laura <milanbartky@gmail.com>
2023-09-07 17:16:11 +0200

    yes

--- app/Http/Controllers/AdvancedSearchController.php
+++ app/Http/Controllers/AdvancedSearchController.php
@@ -488,13 +488,7 @@ class AdvancedSearchController extends Controller
             ->get()->toArray();
 
         if (empty($data)) {
-            $data = WeaponDamageEvent::query()
-                ->select([DB::raw('COUNT(DISTINCT license_identifier) as count'), 'weapon_damage', 'ban_hash'])
-                ->leftJoin('user_bans', 'identifier', '=', 'license_identifier')
-                ->whereIn('weapon_type', [$hash, $unsigned])
-                ->where('hit_players', '!=', '[]')
-                ->groupBy(['weapon_damage', 'ban_hash'])
-                ->get()->toArray();
+            return $this->json(false, null, 'No data');
         }
 
         $dmgBanned = [];
