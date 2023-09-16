5c11a256710c000d679166af07cc8525570d8d20
Laura <milanbartky@gmail.com>
2023-09-17 01:31:11 +0200

    yep

--- resources/js/Pages/Logs/Phone.vue
+++ resources/js/Pages/Logs/Phone.vue
@@ -113,13 +113,13 @@ import Layout from './../../Layouts/App';
 import VSection from './../../Components/Section';
 import Pagination from './../../Components/Pagination';
 
-// bg-red-300 dark:bg-red-900 border-red-500
-// bg-yellow-300 dark:bg-yellow-900 border-yellow-500
-// bg-green-300 dark:bg-green-900 border-green-500
-// bg-blue-300 dark:bg-blue-900 border-blue-500
-// bg-purple-300 dark:bg-purple-900 border-purple-500
-// bg-pink-300 dark:bg-pink-900 border-pink-500
-// bg-lime-300 dark:bg-lime-900 border-lime-500
+// bg-red-300 dark:bg-red-900 border-red-500 text-red-500
+// bg-yellow-300 dark:bg-yellow-900 border-yellow-500 text-yellow-500
+// bg-green-300 dark:bg-green-900 border-green-500 text-green-500
+// bg-blue-300 dark:bg-blue-900 border-blue-500 text-blue-500
+// bg-purple-300 dark:bg-purple-900 border-purple-500 text-purple-500
+// bg-pink-300 dark:bg-pink-900 border-pink-500 text-pink-500
+// bg-lime-300 dark:bg-lime-900 border-lime-500 text-lime-500
 const colors = ['red', 'yellow', 'green', 'blue', 'purple', 'pink', 'lime'],
     usedColors = {};
 
