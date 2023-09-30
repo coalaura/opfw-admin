0dca6aca8dfceb138ada7867e242a5edb7b6a207
Laura <milanbartky@gmail.com>
2023-09-30 13:58:45 +0200

    update cron.sh

--- cron.sh
+++ cron.sh
@@ -1,37 +1,16 @@
 #!/bin/bash
 
 # Path to your instance without trailing slash
-rootDir=/var/www/legacy-rp-admin-v3
+cd /var/www/opfw-admin
 
-# $1 is the cluster (c3, c2, etc.)
-# $2 is the dev-token
-runCronJobs() {
-    echo -n "Running jobs for $1..."
-    echo -n $(curl -s https://$1.legacy-roleplay.com/cron/bans?token=$2)"..."
-    echo $(curl -s https://$1.legacy-roleplay.com/cron/economy?token=$2)
-}
+for directory in ./envs/*/; do
+    [ -L "${d%/}" ] && continue
 
-# $1 is the directory
-countFiles() {
-    ls $1 -1 | wc -l
-}
+    cluster="$(basename -- $directory)"
 
-# For each of your instances copy this
-runCronJobs "c3" "my-dev-token"
+    echo "Running $cluster cronjobs"
 
+    php artisan cron --cluster=$cluster
+done
 
-# Automatically delete log files older than 14 days
-before=$(countFiles $rootDir/storage/logs/)
-
-find $rootDir/storage/logs/ -mindepth 1 -type f -mtime +14 -delete
-
-after=$(countFiles $rootDir/storage/logs/)
-echo "Removed "$(($before - $after))" log files"
-
-# Automatically delete sessions older than 2 days
-before=$(countFiles $rootDir/storage/framework/session_storage)
-
-find $rootDir/storage/framework/session_storage -mindepth 1 -type f -mtime +2 -delete
-
-after=$(countFiles $rootDir/storage/framework/session_storage)
-echo "Removed "$(($before - $after))" .session files"
+echo "Done"
