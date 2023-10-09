6d3e30ea83b733e9b29e102ae010abd434ea0893
Laura <milanbartky@gmail.com>
2023-10-09 14:57:45 +0200

    better maintenance screen

--- app/Http/Middleware/UpdateMiddleware.php
+++ app/Http/Middleware/UpdateMiddleware.php
@@ -12,8 +12,6 @@ use Illuminate\Http\Request;
  */
 class UpdateMiddleware
 {
-    const Style = 'body,html{margin:0;background:#111827;height:100%;width:100%}div{color:#fff;font-family:Nunito,ui-sans-serif,system-ui,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,Noto Sans,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji;position:absolute;top:50%;left:50%;transform:translate(-50%,-50%);font-size:28px;font-weight:600;text-shadow:0 0 4px #000;text-align:center}small{display:block;font-size:16px;font-weight:500;font-style:italic;color:#cecece}small:last-child{font-size:13px;color:#b2e68e}';
-
     /**
      * Handle an incoming request.
      *
@@ -26,9 +24,18 @@ class UpdateMiddleware
         $file = __DIR__ . '/../../../update';
 
         if (file_exists($file)) {
+            $accepts = isset($_SERVER['HTTP_ACCEPT']) ? $_SERVER['HTTP_ACCEPT'] : false;
+
+            if ($accepts === 'application/json') {
+                die(json_encode([
+                    'status' => file_get_contents($file),
+                    'time' => filemtime($file)
+                ]));
+            }
+
             http_response_code(418);
 
-            die('<style>' . self::Style . '</style><div>Update in progress<small>Try again in a few minutes</small><small>Stage: ' . file_get_contents($file) . '</small></div>');
+            die(file_get_contents(__DIR__ . '/../../../public/maintenance.html'));
         }
 
         return $next($request);
