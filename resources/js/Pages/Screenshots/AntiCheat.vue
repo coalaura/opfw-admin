f3425767291fd1789d3b313c7ea17996a9b150eb
Laura <milanbartky@gmail.com>
2023-10-16 16:44:41 +0200

    ok

--- resources/js/Pages/Screenshots/AntiCheat.vue
+++ resources/js/Pages/Screenshots/AntiCheat.vue
@@ -308,7 +308,7 @@ export default {
                     if (metadata.distance === undefined) return false;
 
                     const closestBlip = metadata.data?.closestBlip,
-                        suffix = closestBlip ? ` - *${closestBlip.label} from ${closestBlip.distance.toFixed(1)}m*` : '';
+                        suffix = closestBlip ? ` - *${closestBlip.distance.toFixed(1)}m from ${closestBlip.label}*` : '';
 
                     return `**${metadata.distance.toFixed(2)}m**${suffix}`;
                 case 'bad_screen_word':
