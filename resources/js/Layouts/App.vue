43ed74837e58162cd14882941988e66cff60b0ff
Laura <milanbartky@gmail.com>
2023-11-12 22:09:26 +0100

    yes

--- resources/js/Layouts/App.vue
+++ resources/js/Layouts/App.vue
@@ -80,7 +80,7 @@ export default {
             mapTitle += ' (' + $("#server").val() + ')';
         }
 
-        const title = mapTitle || $("header h1").text().trim();
+        const title = mapTitle || $("header h1").html().replace(/<(\w+) .+?>.*?<\/\1>/g, "").trim();
 
         if (title) {
             const cluster = this.$page?.auth?.cluster ? this.$page.auth.cluster.toUpperCase() : "OP-FW";
