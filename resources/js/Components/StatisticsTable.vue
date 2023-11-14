2692a49e95ba817f6addad12144864e5b1846bcf
Laura <milanbartky@gmail.com>
2023-11-14 16:08:41 +0100

    trend

--- resources/js/Components/StatisticsTable.vue
+++ resources/js/Components/StatisticsTable.vue
@@ -19,9 +19,11 @@
                     <th class="font-semibold px-2 py-0.5 text-left">{{ t('statistics.amount') }}</th>
                     <th class="font-semibold px-2 py-0.5 text-left">{{ t('statistics.difference') }}</th>
                     <th class="font-semibold px-2 py-0.5 text-left">{{ t('statistics.average') }}</th>
+                    <th class="font-semibold px-2 py-0.5 text-left">{{ t('statistics.trend') }}</th>
                 </tr>
 
                 <tr class="border-t border-gray-500" v-if="collapsed">
+                    <td class="px-2 py-0.5">...</td>
                     <td class="px-2 py-0.5">...</td>
                     <td class="px-2 py-0.5">...</td>
                     <td class="px-2 py-0.5">...</td>
@@ -35,6 +37,7 @@
                     <td class="px-2 py-0.5">{{ numberFormat(entry.amount, false, true) }}</td>
                     <td class="px-2 py-0.5" v-html="previous(index, 'amount')"></td>
                     <td class="px-2 py-0.5">{{ entry.count > 0 ? numberFormat(entry.amount / entry.count, false, true) : '-' }}</td>
+                    <td class="px-2 py-0.5 font-semibold" v-html="trend(index, 'amount')"></td>
                 </tr>
             </table>
         </div>
@@ -72,20 +75,36 @@ export default {
         };
     },
     methods: {
-        previous(index, key) {
-            if (index >= this.data.length) return '-';
+        getDiffToPrevious(index, key) {
+            if (index >= this.data.length - 1) return null;
 
             const before = this.data[index + 1][key],
                 after = this.data[index][key];
 
-            const diff = after - before,
-                format = this.numberFormat(diff, false, true);
+            return after - before;
+        },
+        previous(index, key) {
+            const diff = this.getDiffToPrevious(index, key);
 
-            if (diff < 0) return `<i class="text-red-700 dark:text-red-300">${format}</i>`;
+            if (diff === null) return '-';
+
+            const format = this.numberFormat(diff, false, true);
+
+            if (diff < 0) return `<span class="text-red-700 dark:text-red-300">${format}</span>`;
             else if (diff > 0) return `<span class="text-green-700 dark:text-green-300">+${format}</span>`;
 
             return '-';
-        }
+        },
+        trend(index, key) {
+            const diff = this.getDiffToPrevious(index, key);
+
+            if (diff === null) return '-';
+
+            if (diff < 0) return `<span class="text-red-700 dark:text-red-300">↘</span>`;
+            else if (diff > 0) return `<span class="text-green-700 dark:text-green-300">↗</span>`;
+
+            return `<span class="text-blue-700 dark:text-blue-300">=</span>`;
+        },
     }
 }
 </script>
