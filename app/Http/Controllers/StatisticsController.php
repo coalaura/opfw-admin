d07b16ec71141ed745f6fbaeb9d5b924f16a4d57
Laura <milanbartky@gmail.com>
2023-11-14 14:28:13 +0100

    remove stats

--- app/Http/Controllers/StatisticsController.php
+++ app/Http/Controllers/StatisticsController.php
@@ -21,18 +21,6 @@ class StatisticsController extends Controller
         $license = license();
 
         return Inertia::render('Statistics/Index', [
-            'bans'           => StatisticsHelper::getBanStats(),
-            'warnings'       => StatisticsHelper::getWarningStats(),
-            'notes'          => StatisticsHelper::getNoteStats(),
-            'creations'      => StatisticsHelper::getCharacterCreationStats(),
-            'deletions'      => StatisticsHelper::getCharacterDeletionStats(),
-            'userStatistics' => StatisticsHelper::getUserStatistics(),
-            'luckyWheel'     => StatisticsHelper::getLuckyWheelStats(),
-            'blackjack'      => StatisticsHelper::getBlackjackStats($license),
-            'tracks'         => StatisticsHelper::getTracksStats($license),
-            'slots'          => StatisticsHelper::getSlotsStats($license),
-
-            'casinoRevenue'  => StatisticsHelper::getCasinoRevenueStats(),
         ]);
     }
 
