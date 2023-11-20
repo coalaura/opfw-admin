1c130dfc8ace41164fd12f9bd78a0eabda22c7b9
Laura <milanbartky@gmail.com>
2023-11-20 22:20:17 +0100

    cache

--- app/Http/Controllers/StatisticsController.php
+++ app/Http/Controllers/StatisticsController.php
@@ -6,6 +6,7 @@ use App\Helpers\StatisticsHelper;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Inertia\Response;
+use App\Helpers\CacheHelper;
 
 class StatisticsController extends Controller
 {
@@ -28,37 +29,47 @@ class StatisticsController extends Controller
     {
         $result = false;
 
-        switch ($source) {
-            // Currency statistics
-            case 'pdm':
-                $result = StatisticsHelper::collectPDMStatistics();
-                break;
-            case 'edm':
-                $result = StatisticsHelper::collectEDMStatistics();
-                break;
-            case 'gem':
-                $result = StatisticsHelper::collectGemSaleStatistics();
-                break;
-            case 'pawn':
-                $result = StatisticsHelper::collectPawnshopStatistics();
-                break;
-            case 'casino':
-                $result = StatisticsHelper::collectCasinoStatistics();
-                break;
-            case 'drugs':
-                $result = StatisticsHelper::collectDrugSaleStatistics();
-                break;
-            case 'store':
-                $result = StatisticsHelper::collectStoreSaleStatistics();
-                break;
-            case 'paycheck':
-                $result = StatisticsHelper::collectPaycheckStatistics();
-                break;
+        $key = "statistics.{$source}";
 
-            // Non currency statistics
-            case 'robberies':
-                $result = StatisticsHelper::collectRobberiesStatistics();
-                break;
+        if (CacheHelper::exists($key)) {
+            $result = CacheHelper::read($key) ?? false;
+        }
+
+        if (!$result) {
+            switch ($source) {
+                // Currency statistics
+                case 'pdm':
+                    $result = StatisticsHelper::collectPDMStatistics();
+                    break;
+                case 'edm':
+                    $result = StatisticsHelper::collectEDMStatistics();
+                    break;
+                case 'gem':
+                    $result = StatisticsHelper::collectGemSaleStatistics();
+                    break;
+                case 'pawn':
+                    $result = StatisticsHelper::collectPawnshopStatistics();
+                    break;
+                case 'casino':
+                    $result = StatisticsHelper::collectCasinoStatistics();
+                    break;
+                case 'drugs':
+                    $result = StatisticsHelper::collectDrugSaleStatistics();
+                    break;
+                case 'store':
+                    $result = StatisticsHelper::collectStoreSaleStatistics();
+                    break;
+                case 'paycheck':
+                    $result = StatisticsHelper::collectPaycheckStatistics();
+                    break;
+
+                // Non currency statistics
+                case 'robberies':
+                    $result = StatisticsHelper::collectRobberiesStatistics();
+                    break;
+            }
+
+            CacheHelper::write($key, $result, CacheHelper::HOUR);
         }
 
         return $this->json(true, $result);
