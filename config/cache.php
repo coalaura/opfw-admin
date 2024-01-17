75df0c08628c31eefc4e1348a2f9b6403b92966a
Laura <milanbartky@gmail.com>
2024-01-17 14:27:49 +0100

    Refactor cache configuration

--- config/cache.php
+++ config/cache.php
@@ -16,9 +16,9 @@ return [
     | Supported: "apc", "array", "database", "file",
     |            "memcached", "redis", "dynamodb"
     |
-    */
+     */
 
-    'default' => 'array',//env('CACHE_DRIVER', 'array'),
+    'default' => 'array', //env('CACHE_DRIVER', 'array'),
 
     /*
     |--------------------------------------------------------------------------
@@ -29,59 +29,60 @@ return [
     | well as their drivers. You may even define multiple stores for the
     | same cache driver to group types of items stored in your caches.
     |
-    */
+     */
 
     'stores' => [
 
-        'apc' => [
+        'apc'       => [
             'driver' => 'apc',
         ],
 
-        'array' => [
+        'array'     => [
             'driver' => 'array',
         ],
 
-        'database' => [
-            'driver' => 'database',
-            'table' => 'cache',
+        'database'  => [
+            'driver'     => 'database',
+            'table'      => 'cache',
             'connection' => null,
         ],
 
-        'file' => [
-            'driver' => 'file',
-            'path' => storage_path('framework/cache/data'),
+        'file'      => [
+            'driver'     => 'file',
+            'path'       => storage_path('framework/cache/data'),
+            'permission' => 0664,
         ],
 
         'memcached' => [
-            'driver' => 'memcached',
+            'driver'        => 'memcached',
             'persistent_id' => env('MEMCACHED_PERSISTENT_ID'),
-            'sasl' => [
+            'sasl'          => [
                 env('MEMCACHED_USERNAME'),
                 env('MEMCACHED_PASSWORD'),
             ],
-            'options' => [
+            'options'       => [
                 // Memcached::OPT_CONNECT_TIMEOUT => 2000,
             ],
-            'servers' => [
+            'servers'       => [
                 [
-                    'host' => env('MEMCACHED_HOST', '127.0.0.1'),
-                    'port' => env('MEMCACHED_PORT', 11211),
+                    'host'   => env('MEMCACHED_HOST', '127.0.0.1'),
+                    'port'   => env('MEMCACHED_PORT', 11211),
                     'weight' => 100,
                 ],
             ],
         ],
 
-        'redis' => [
-            'driver' => 'redis',
+        'redis'     => [
+            'driver'     => 'redis',
             'connection' => 'cache',
         ],
 
-        'dynamodb' => [
-            'driver' => 'dynamodb',
-            'key' => env('AWS_ACCESS_KEY_ID'),
-            'secret' => env('AWS_SECRET_ACCESS_KEY'),
-            'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
-            'table' => env('DYNAMODB_CACHE_TABLE', 'cache'),
+        'dynamodb'  => [
+            'driver'   => 'dynamodb',
+            'key'      => env('AWS_ACCESS_KEY_ID'),
+            'secret'   => env('AWS_SECRET_ACCESS_KEY'),
+            'region'   => env('AWS_DEFAULT_REGION', 'us-east-1'),
+            'table'    => env('DYNAMODB_CACHE_TABLE', 'cache'),
             'endpoint' => env('DYNAMODB_ENDPOINT'),
         ],
 
@@ -96,8 +97,8 @@ return [
     | be other applications utilizing the same cache. So, we'll specify a
     | value to get prefixed to all our keys so we can avoid collisions.
     |
-    */
+     */
 
-    'prefix' => env('CACHE_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_cache'),
+    'prefix'  => env('CACHE_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_cache'),
 
 ];
