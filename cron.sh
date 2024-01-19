53018e8c9ff4f89bf9f7f2d22da29739c6b2fb70
Laura <milanbartky@gmail.com>
2024-01-20 00:28:44 +0100

    fix cron script

--- cron.sh
+++ cron.sh
@@ -18,4 +18,9 @@ for directory in ./envs/*/; do
     php artisan cron --cluster=$cluster
 done
 
+# Repair file permissions
+chown -R www-data:www-data /var/www/opfw-admin/storage
+chgrp -R www-data /var/www/opfw-admin/storage
+chmod -R ug+rwx /var/www/opfw-admin/storage
+
 echo "Done"
