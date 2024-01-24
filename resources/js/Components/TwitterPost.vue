e97c065a918758811156798484815690e277181c
Laura <milanbartky@gmail.com>
2024-01-24 12:51:07 +0100

    yes

--- resources/js/Components/TwitterPost.vue
+++ resources/js/Components/TwitterPost.vue
@@ -189,7 +189,7 @@ export default {
             body = body.trim();
 
             if (body.match(/^https?:\/\/[^\s]+?\.(png|jpe?g|gif|bmp|webp)(\?[^\s]*)?$/i)) {
-                return '<a href="' + body + '" target="_blank" class="block max-w-screen-sm w-full h-twitter-img overflow-hidden rounded-lg"><img src="' + body + '" class="block w-full h-full object-cover" /></a>';
+                return '<a href="' + body + '" target="_blank" class="block max-w-screen-sm w-full h-twitter-img overflow-hidden rounded-lg"><img src="' + body + '" class="block w-full h-full object-cover translate hover:scale-105" /></a>';
             }
 
             return body;
