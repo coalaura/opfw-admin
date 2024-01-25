38fe93fc13b1076cef34b40688c8d786236cb5be
Laura <milanbartky@gmail.com>
2024-01-25 23:57:51 +0100

    ye

--- database/migrations/2024_01_25_115500_update_panel_ip_infos_table.php
+++ database/migrations/2024_01_25_115500_update_panel_ip_infos_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class UpdateTwitterAccountsTable extends Migration
+class UpdatePanelIpInfosTable extends Migration
 {
 	/**
 	 * Run the migrations.
