921c0f2010934414b302ba88d9d0b181927dbd7c
Laura <milanbartky@gmail.com>
2024-02-07 17:16:30 +0100

    yes

--- resources/js/Pages/Screenshots/AntiCheat.vue
+++ resources/js/Pages/Screenshots/AntiCheat.vue
@@ -50,10 +50,10 @@
                                         {{ screenshot.player_name }}
                                     </inertia-link>
                                 </td>
-                                <td class="p-3 mobile:block italic text-gray-600 dark:text-gray-400 text-sm" colspan="4">
+                                <td class="p-3 mobile:block italic text-gray-600 dark:text-gray-400 text-xs" colspan="4">
                                     Banned indefinitely for <span class="font-semibold" :class="{'cursor-help': screenshot.details !== screenshot.reason}" @click="screenshot.details = screenshot.reason">{{ screenshot.details }}</span>
                                 </td>
-                                <td class="p-3 pr-8 mobile:block italic text-gray-600 dark:text-gray-400 w-60 whitespace-nowrap">
+                                <td class="p-3 pr-8 mobile:block italic text-gray-600 dark:text-gray-400 w-60 text-sm whitespace-nowrap">
                                     {{ screenshot.timestamp * 1000 | formatTime(true) }}
                                 </td>
                             </template>
