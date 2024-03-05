4b4167909374b4b275a960dc9199d8ea4894f395
Laura <milanbartky@gmail.com>
2024-03-05 17:25:31 +0100

    yes

--- routes/console.php
+++ routes/console.php
@@ -1,4 +1,6 @@
 <?php
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Artisan;
 
 /*
 |--------------------------------------------------------------------------
@@ -10,3 +12,62 @@
 | simple approach to interacting with each command"s IO methods.
 |
  */
+
+$disabledProdCommands = [
+    'ui',
+    'tinker',
+
+    // db:*
+    'db:seed',
+    'db:wipe',
+
+    // make:*
+    'make:cast',
+    'make:channel',
+    'make:command',
+    'make:component',
+    'make:controller',
+    'make:event',
+    'make:exception',
+    'make:factory',
+    'make:job',
+    'make:listener',
+    'make:mail',
+    'make:middleware',
+    'make:migration',
+    'make:model',
+    'make:notification',
+    'make:observer',
+    'make:policy',
+    'make:provider',
+    'make:request',
+    'make:resource',
+    'make:rule',
+    'make:seeder',
+    'make:test',
+
+    // migrate:*
+    'migrate:fresh',
+    'migrate:install',
+
+    // session:*
+    'session:table',
+
+    // stub:*
+    'stub:publish',
+
+    // ui:*
+    'ui:auth',
+    'ui:controllers',
+
+    // vendor:*
+    'vendor:publish',
+];
+
+if ('production' === App::environment()) {
+    foreach ($disabledProdCommands as $command) {
+        Artisan::command($command, function () {
+            $this->comment('This command is disabled in production.');
+        })->describe('Override default command in production.');
+    }
+}
