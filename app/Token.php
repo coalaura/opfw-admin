d0cad316b31072bc77d8e735f724665f65213eff
Laura <milanbartky@gmail.com>
2024-04-01 14:22:57 +0200

    yes

--- app/Token.php
+++ app/Token.php
@@ -78,6 +78,7 @@ class Token extends Model
             $query->where('id', '<', $beforeId);
         }
 
+        /*
         $panelToken = env('OP_FW_TOKEN', '');
 
         if ($panelToken) {
@@ -87,6 +88,7 @@ class Token extends Model
                 $query->where('token_id', '!=', $panelTokenId);
             }
         }
+        */
 
         return $query->get()->toArray();
     }
