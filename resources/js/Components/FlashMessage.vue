4e8deb3cfd5474c5bb0b1d84f1ddb150b8b70a74
Laura <milanbartky@gmail.com>
2024-04-19 21:55:04 +0200

    ye

--- resources/js/Components/FlashMessage.vue
+++ resources/js/Components/FlashMessage.vue
@@ -1,14 +1,14 @@
 <template>
     <div v-if="show" class="mt-2">
         <!-- Success -->
-        <alert v-if="$page.flash.success" class="bg-green-500 mb-6">
+        <alert v-if="$page.flash.success" class="bg-green-500 mb-4">
             <p>
                 {{ $page.flash.success }}
             </p>
         </alert>
 
         <!-- Error -->
-        <alert v-if="$page.flash.error" class="bg-red-500 mb-6">
+        <alert v-if="$page.flash.error" class="bg-red-500 mb-4">
             <p>
                 {{ $page.flash.error }}
             </p>
