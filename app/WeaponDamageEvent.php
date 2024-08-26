a710599f7635a479cac04146bd2b014c99160620
Laura <milanbartky@gmail.com>
2024-08-26 16:10:23 +0200

    fix

--- app/WeaponDamageEvent.php
+++ app/WeaponDamageEvent.php
@@ -38,50 +38,27 @@ class WeaponDamageEvent extends Model
     ];
 
     const HitComponents = [
-        // Confirmed (very sure):
-        0  => "center-mass",
-        1  => "upper left leg",
-        2  => "lower left leg",
-        3  => "left foot",
-        4  => "upper right leg",
-        5  => "lower right leg",
-        6  => "right foot",
-        7  => "lower spine",
-        8  => "stomach",
-        9  => "upper chest",
-        10 => "upper chest",
-        11 => "left shoulder",
-        12 => "upper left arm",
-        13 => "lower left arm",
-        14 => "left hand",
-        15 => "right shoulder",
-        16 => "upper right arm",
-        17 => "lower right arm",
-        18 => "right hand",
-        19 => "neck",
-        20 => "head",
-
-        // Unknown/Unsure:
-        21 => "unknown (21)",
-        22 => "unknown (22)",
-    ];
-
-    const DamageTypes = [
-        0  => "unknown (0)",
-        1  => "no damage",
-        2  => "melee",
-        3  => "bullet",
-        4  => "force ragdoll fall",
-        5  => "explosive",
-        6  => "fire",
-        //7 => "",
-        8  => "fall",
-        //9 => "",
-        10 => "electric",
-        11 => "barbed wire",
-        12 => "extinguisher",
-        13 => "gas",
-        14 => "water cannon",
+        0  => "Buttocks",
+        1  => "Left Thigh",
+        2  => "Left Shin",
+        3  => "Left Foot",
+        4  => "Right Thigh",
+        5  => "Right Shin",
+        6  => "Right Foot",
+        7  => "Spine0",
+        8  => "Spine1",
+        9  => "Spine2",
+        10 => "Spine3",
+        11 => "Left Clavicle",
+        12 => "Left Upper Arm",
+        13 => "Left Lower Arm",
+        14 => "Left Hand",
+        15 => "Right Clavicle",
+        16 => "Right Upper Arm",
+        17 => "Right Lower Arm",
+        18 => "Right Hand",
+        19 => "Neck",
+        20 => "Head",
     ];
 
     public static function getWeaponList(): array
@@ -103,7 +80,7 @@ class WeaponDamageEvent extends Model
             return self::HitComponents[$component];
         }
 
-        return "undiscovered ($component)";
+        return "unknown ($component)";
     }
 
     public static function getDamageWeapon($hash)
