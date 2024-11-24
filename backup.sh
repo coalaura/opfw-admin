f982136394e4104b267832f13a0e5c0be8e2f2f9
Laura <milanbartky@gmail.com>
2024-11-25 00:07:20 +0100

    add backup script

---
+++ backup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Creating backup of files..."
+filename=$(date +%d%m%Y_%H%M%S)
+
+# Deprioritizes the processes for both CPU time and io usage
+# It was eating up resources every time making the panel slow af
+nice -n 19 ionice -c3 tar -cjf panel_$filename.tar.bz2 public/_discord_attachments public/_transcripts public/_uploads
+
+echo "Backup complete!"
