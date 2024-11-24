fe02c509b05afbc5c378aabebabbf94c21d0265f
Laura <milanbartky@gmail.com>
2024-11-25 00:13:26 +0100

    yes

--- backup.sh
+++ backup.sh
@@ -5,6 +5,6 @@ filename=$(date +%d%m%Y_%H%M%S)
 
 # Deprioritizes the processes for both CPU time and io usage
 # It was eating up resources every time making the panel slow af
-nice -n 19 ionice -c3 tar --totals -cjf panel_$filename.tar.bz2 public/_discord_attachments public/_transcripts public/_uploads
+nice -n 19 ionice -c3 tar --totals -cjf panel_$filename.tar.zstd public/_discord_attachments public/_transcripts public/_uploads
 
 echo "Backup complete!"
