7a961f59b457942bef3e40f5fe6063ebb3f8d666
Laura <milanbartky@gmail.com>
2024-11-25 00:20:30 +0100

    fix

--- backup.sh
+++ backup.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-echo "Creating backup of files..."
+echo "Creating archive of files..."
+tar -cf files.tar public/_discord_attachments public/_transcripts public/_uploads
+
+echo "Creating compressed backup..."
 filename=$(date +%d%m%Y_%H%M%S)
 
 # Deprioritizes the processes for both CPU time and io usage
 # It was eating up resources every time making the panel slow af
-nice -n 19 ionice -c3 tar --totals -cjf panel_$filename.tar.zstd public/_discord_attachments public/_transcripts public/_uploads
+nice -n 19 zstd -T0 files.tar -o panel_$filename.zst
 
 echo "Backup complete!"
