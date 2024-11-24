499f402e20d76a54dd5084a6e53bd23faf74a00c
Laura <milanbartky@gmail.com>
2024-11-25 00:22:32 +0100

    fix

--- backup.sh
+++ backup.sh
@@ -8,6 +8,6 @@ filename=$(date +%d%m%Y_%H%M%S)
 
 # Deprioritizes the processes for both CPU time and io usage
 # It was eating up resources every time making the panel slow af
-nice -n 19 zstd -T0 files.tar -o panel_$filename.zst
+nice -n 19 ionice -c3 tar -cjf panel_$filename.tar.bz2 files.tar
 
 echo "Backup complete!"
