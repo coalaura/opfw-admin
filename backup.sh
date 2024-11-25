b401002320ad6be47fed1f466f07fb15fcea780b
Laura <milanbartky@gmail.com>
2024-11-25 16:22:32 +0100

    yes

--- backup.sh
+++ backup.sh
@@ -6,9 +6,7 @@ tar -cf files.tar public/_discord_attachments public/_transcripts public/_upload
 echo "Creating compressed backup..."
 filename=$(date +%d%m%Y_%H%M%S)
 
-# Deprioritizes the processes for both CPU time and io usage
-# It was eating up resources every time making the panel slow af
-nice -n 19 ionice -c3 zstd -r -T0 files.tar -o panel_$filename.zst
+zstd -r -T0 files.tar -o panel_$filename.zst
 
 rm files.tar
 
