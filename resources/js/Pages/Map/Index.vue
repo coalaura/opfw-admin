eed7c2b5e1d625653858fd33796080046db8fbab
Laura <milanbartky@gmail.com>
2025-01-12 21:31:46 +0100

    yes

--- resources/js/Pages/Map/Index.vue
+++ resources/js/Pages/Map/Index.vue
@@ -41,9 +41,11 @@
                 </button>
 
                 <!-- Show Timestamp -->
-                <button class="p-2 w-11 text-center font-semibold text-white rounded bg-blue-600 dark:bg-blue-500" :title="t('map.timestamp_title')" @click="isTimestamp = true" v-if="this.perm.check(this.perm.PERM_ADVANCED)">
-                    <i class="fas fa-vial"></i>
-                </button>
+                <!-- This shit breaks all the time idk if i cba to fix it or just remove it permanently
+                    <button class="p-2 w-11 text-center font-semibold text-white rounded bg-blue-600 dark:bg-blue-500" :title="t('map.timestamp_title')" @click="isTimestamp = true" v-if="this.perm.check(this.perm.PERM_ADVANCED)">
+                        <i class="fas fa-vial"></i>
+                    </button>
+                -->
 
                 <!-- Show Historic -->
                 <button class="p-2 w-11 text-center font-semibold text-white rounded bg-blue-600 dark:bg-blue-500" :title="t('map.historic_title')" @click="showHistoric()" v-if="this.perm.check(this.perm.PERM_ADVANCED)">
