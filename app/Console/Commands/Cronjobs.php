478d225cfd7dfda466cb70298e68d22fd0b684af
Laura <milanbartky@gmail.com>
2025-05-07 06:05:22 +0200

    yes

--- app/Console/Commands/Cronjobs.php
+++ app/Console/Commands/Cronjobs.php
@@ -26,6 +26,7 @@ class Cronjobs extends Command
         [ServerAPI::class, 'getChatEmotes'],
         [ServerAPI::class, 'getRoutes'],
         [ServerAPI::class, 'getCrafting'],
+        [ServerAPI::class, 'getConfig'],
     ];
 
     /**
