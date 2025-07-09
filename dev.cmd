3e1068560e8438fdd53806e7d4806540db1bcedd
Laura <milanbartky@gmail.com>
2025-07-09 03:49:40 +0200

    yes

--- dev.cmd
+++ dev.cmd
@@ -4,16 +4,4 @@ php artisan config:clear
 php artisan view:clear
 php artisan route:clear
 
-if EXIST ..\admin-panel-socket-js (
-	echo %base%
-
-	pushd ..\admin-panel-socket-js
-
-	start bun main.js
-
-	popd
-)
-
-start bun run dev
-
 localhost -ro -c "..\.https\localhost.pem" -k "..\.https\localhost-key.pem"
