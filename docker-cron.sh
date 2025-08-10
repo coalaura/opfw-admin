09aa1193b3f2c537b22c368d4a4547707ea268cc
Laura <milanbartky@gmail.com>
2025-08-10 17:54:44 +0200

    logs

--- docker-cron.sh
+++ docker-cron.sh
@@ -2,9 +2,12 @@
 
 cd /var/www/html
 
+echo "Running cronjobs..."
 php artisan cron
 
 # Repair file permissions
 chown -R www-data:www-data storage
 chgrp -R www-data storage
 chmod -R ug+rwx storage
+
+echo "Finished cronjobs."
