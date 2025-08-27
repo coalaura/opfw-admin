7d263d4373b1b7792ec5c632a69fb7e5bf5bf06d
Laura <milanbartky@gmail.com>
2025-08-27 12:03:37 +0200

    yes

--- app/Helpers/DeviceHelper.php
+++ app/Helpers/DeviceHelper.php
@@ -156,9 +156,8 @@ class DeviceHelper
     }
 }
 
-//*
+/*
 $test = <<<EOF
-["audioinput_hodetelefonmikrofon_xbox_controller_10f5_0043","audioinput_stereo_miks_realtek_r_audio","videoinput_lsvcam","videoinput_bytecast_virtualcamera1","videoinput_bytecast_virtualcamera2","videoinput_bytecast_virtualcamera3","videoinput_bytecast_virtualcamera4","audiooutput_hodetelefoner_xbox_controller_10f5_0043","audiooutput_h_yttalere_realtek_r_audio"]
 EOF;
 
 if (strpos($test, "[") === false) {
