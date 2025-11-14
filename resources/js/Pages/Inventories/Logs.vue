1cadd1aaefa71d645d6bdd2d12e8a9b0911cb6dd
Laura <milanbartky@gmail.com>
2025-11-14 16:22:36 +0100

    fix

--- resources/js/Pages/Inventories/Logs.vue
+++ resources/js/Pages/Inventories/Logs.vue
@@ -215,12 +215,20 @@ export default {
 			return items;
 		},
 		fromInventory(details) {
-			const inventory = details.match(/(?<=from inventory )[\w-:]+/i).pop();
+			const inventory = details.match(/(?<=from inventory )[\w-:]+/i)?.pop();
+
+			if (!inventory) {
+				return "N/A";
+			}
 
 			return `<a title="${this.t('inventories.show_inv')}" class="text-indigo-600 dark:text-indigo-400 font-semibold" href="/inventory/${inventory.replace(/:\d+/, '')}">${inventory}</a>`;
 		},
 		toInventory(details) {
-			const inventory = details.match(/(?<=to )[\w-:]+/i).pop();
+			const inventory = details.match(/(?<=to )[\w-:]+/i)?.pop();
+
+			if (!inventory) {
+				return "N/A";
+			}
 
 			return `<a title="${this.t('inventories.show_inv')}" class="text-indigo-600 dark:text-indigo-400 font-semibold" href="/inventory/${inventory.replace(/:\d+/, '')}">${inventory}</a>`;
 		},
