b0e69fc3c2e4f73cf528bd95d3a228be20b5752c
Laura <laura@wiese2.org>
2026-05-19 19:13:22 +0200

    yes

--- resources/js/Pages/Tokens/Index.vue
+++ resources/js/Pages/Tokens/Index.vue
@@ -54,8 +54,9 @@
                                 <option v-for="method in methods" :value="method">{{ method }}</option>
                             </select>
 
-                            <select v-model="permission.path" class="px-1 py-0.5 block bg-gray-200 dark:bg-gray-800 text-sm w-full" @change="token.changed = true" :disabled="token.disabled" :multiple="permission.method === 'REST'" :class="{ '!bg-blue-500 !bg-opacity-20 border-blue-400': token.disabled }">
-                                <option value="*" v-if="!routes[permission.method].includes('*')">*</option>
+                            <input type="text" maxlength="512" v-model="permission.path" class="px-1 py-0.5 block bg-gray-200 dark:bg-gray-800 text-sm w-full" v-if="permission.method === 'REST'" placeholder="characters{first_name,last_name}" @change="token.changed = true" :disabled="token.disabled" :class="{ '!bg-blue-500 !bg-opacity-20 border-blue-400': token.disabled }" />
+                            <select v-model="permission.path" class="px-1 py-0.5 block bg-gray-200 dark:bg-gray-800 text-sm w-full" @change="token.changed = true" :disabled="token.disabled" v-else :class="{ '!bg-blue-500 !bg-opacity-20 border-blue-400': token.disabled }">
+                                <option value="*">*</option>
 
                                 <option v-for="path in routes[permission.method]" :value="path">{{ path }}</option>
                             </select>
